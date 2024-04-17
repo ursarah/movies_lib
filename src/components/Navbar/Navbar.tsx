fc3319c3479fd49f3c50e704471a68235d389404
ursarah <sarahurcezino@gmail.com>
2024-04-17 15:14:30 -0300

    feat: use navigate and submit the results

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,20 @@
 import { BiCameraMovie, BiSearchAlt } from "react-icons/bi";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+  };
+
   return (
     <div id='navbar'>
       <h2>
@@ -10,8 +22,13 @@ const Navbar = () => {
           <BiCameraMovie /> Movies Lib
         </Link>
       </h2>
-      <form action=''>
-        <input type='text' placeholder='Busque um Filme' />
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Busque um Filme'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
         <button type='submit'>
           <BiSearchAlt />
         </button>
