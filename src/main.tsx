934af582da721e940fb4705909cdce7681def1ee
ursarah <sarahurcezino@gmail.com>
2024-04-17 13:08:05 -0300

    feat: create pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+// Routes
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// Pages
+import Home from "./pages/Home.tsx";
+import Movie from "./pages/Movie.tsx";
+import Search from "./pages/Search.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='movies/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
